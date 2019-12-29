12b76a1a4794537f7f30b91e66b12fb59c295ca0
GotohFumiaki <fgotoh@smaile-lab.com>
2019-12-29 21:11:31 +0900

    位置情報が取れるか現在の状態をテキストにして表示

---
+++ Assets/Script/UIBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+
+public class UIBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // スクリプトを取得
+        var location = GameObject.FindGameObjectWithTag("GameController").GetComponent<UnityLocationClass>();
+        // キャンバスを取得
+        var mainCanvas = GameObject.FindGameObjectWithTag("Canvas");
+        var isLocateText = mainCanvas.GetComponentInChildren<Text>();
+
+        var updateText = this.UpdateAsObservable()
+            .Subscribe(_ => {
+                isLocateText.text = location.IsEnabledByUserToStringProp;
+            });
+    }
+}
--- Assets/Script/UnityLocationClass.cs
+++ Assets/Script/UnityLocationClass.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
 
 public class UnityLocationClass : MonoBehaviour
 {
+    public string IsEnabledByUserToStringProp
+    {
+        get{ return Input.location.isEnabledByUser.ToString(); }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Input.location.Start();
     }
 }
