6a61c0c76305d453598ea26a67e832539852adb4
GotohFumiaki <fgotoh@smaile-lab.com>
2019-12-29 19:47:21 +0900

    UniRx導入

---
+++ Assets/Script/UnityLocationClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityLocationClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
